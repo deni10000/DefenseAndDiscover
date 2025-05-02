2dd96b55730bfc34d435f5b45992c93c1c441166
SKYWALKER <770vda@mail.ru>
2025-05-02 23:34:31 +0300

    remove useless files

--- llm_config/prompts.py
+++
@@ -1,7 +0,0 @@
-from langchain.prompts import PromptTemplate
-
-quiz_prompt = PromptTemplate(
-    input_variables=["topic", "num_questions", "difficulty"],
-    template="""Придумай {num_questions} {difficulty} неповторяющихся вопроса для викторины по теме {topic}. 
-    Выведи результат в виде JSON с полями: question (вопрос), options (4 варианта ответов) и correct_answer (правильный ответ).""",
-)

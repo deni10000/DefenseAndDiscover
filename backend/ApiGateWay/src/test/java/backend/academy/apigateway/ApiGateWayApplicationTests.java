27418d80dec6aa119b339ebc65e832275bd7e276
SKYWALKER <770vda@mail.ru>
2025-05-04 04:44:46 +0300

    add testContainers in APIGateway

--- backend/ApiGateWay/src/test/java/backend/academy/apigateway/ApiGateWayApplicationTests.java
+++ backend/ApiGateWay/src/test/java/backend/academy/apigateway/ApiGateWayApplicationTests.java
@@ -1,11 +1,14 @@
 package backend.academy.apigateway;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApiGateWayApplicationTests {
-    @Test
-    void contextLoads() {
+
+
+    public static void main(String[] args) {
+        SpringApplication.from(ApiGateWayApplication::main).with(TestcontainersConfiguration.class).run(args);
     }
 }

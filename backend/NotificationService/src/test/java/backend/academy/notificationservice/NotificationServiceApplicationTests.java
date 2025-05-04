c6312051f65da914cb6cc91c0351ca92fc3b39d8
SKYWALKER <770vda@mail.ru>
2025-05-04 03:00:08 +0300

    configure tests in NotificationService

--- backend/NotificationService/src/test/java/backend/academy/notificationservice/NotificationServiceApplicationTests.java
+++ backend/NotificationService/src/test/java/backend/academy/notificationservice/NotificationServiceApplicationTests.java
@@ -2,11 +2,16 @@ package backend.academy.notificationservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class NotificationServiceApplicationTests {
+
+    @MockitoBean
+    private JavaMailSender mailSender;
 
     @Test
     void contextLoads() {

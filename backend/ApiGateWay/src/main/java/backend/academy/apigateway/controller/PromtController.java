51a7483d5f98b335479012c9e1749a64f80af9df
SKYWALKER <770vda@mail.ru>
2025-05-05 08:22:16 +0300

    add: add logs

--- backend/ApiGateWay/src/main/java/backend/academy/apigateway/controller/PromtController.java
+++ backend/ApiGateWay/src/main/java/backend/academy/apigateway/controller/PromtController.java
@@ -9,6 +9,7 @@ import backend.academy.apigateway.utils.ApiPaths;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @Tag(name = "Вопросы", description = "Операции с вопросами")
 public class PromtController {
 
@@ -26,12 +28,22 @@ public class PromtController {
     @PostMapping(ApiPaths.BASE_API + "/getQuestion")
     @Operation(summary = "Сгенерировать вопрос")
     public ResponseEntity<List<QuestionDto>> getQuestion(@RequestBody RequestPromtDto requestPromtDto) {
-        return ResponseEntity.ok(promtClient.getQuestion(requestPromtDto));
+        try {
+            return ResponseEntity.ok(promtClient.getQuestion(requestPromtDto));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
     @Operation(summary = "Отправить ответ на вопрос")
     @PostMapping(ApiPaths.BASE_API + "/postAnswer")
     public ResponseEntity<CheckAnswerDto> postAnswer(@RequestBody PostAnswerDto postAnswerDto) {
-        return ResponseEntity.ok(promtClient.postAnswer(postAnswerDto));
+        try {
+            return ResponseEntity.ok(promtClient.postAnswer(postAnswerDto));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }

fb23c142d613502269f4dc434b356f0ecb7ae705
SKYWALKER <770vda@mail.ru>
2025-05-05 03:08:36 +0300

    add: KafkaClientServiceImpl exception handler

--- backend/ApiGateWay/src/main/java/backend/academy/apigateway/service/impl/KafkaClientServiceImpl.java
+++ backend/ApiGateWay/src/main/java/backend/academy/apigateway/service/impl/KafkaClientServiceImpl.java
@@ -6,12 +6,14 @@ import backend.academy.apigateway.service.KafkaClientService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class KafkaClientServiceImpl implements KafkaClientService {
 
     private final KafkaTemplate<Long, String> kafkaTemplate;
@@ -20,8 +22,12 @@ public class KafkaClientServiceImpl implements KafkaClientService {
     @Value("${app.user-confirmation.topic}")
     private String topic;
 
-    public void sendNotificationStackOverflow(UserConfirmation userConfirmation) throws JsonProcessingException {
-        System.out.println("сообщение отправлено");
-        kafkaTemplate.send(topic, objectMapper.writeValueAsString(userConfirmation));
+    public void sendNotificationStackOverflow(UserConfirmation userConfirmation)  {
+        try {
+            System.out.println("сообщение отправлено");
+            kafkaTemplate.send(topic, objectMapper.writeValueAsString(userConfirmation));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
     }
 }
